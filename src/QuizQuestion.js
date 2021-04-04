6eba9f998cebb7e512f317d31f3a81e260f54dd2
Dave <davms@iscte-iul.pt>
2021-04-04 17:29:37 +0100

    PluralSight_2

---
+++ src/QuizQuestion.js
@@ -0,0 +1,25 @@
+import React, {Component } from 'react'
+import Quiz from './Quiz'
+
+class QuizQuestion extends Component {
+  constructor(props){
+  super(props)
+}
+    render() {
+      return (
+        <main>
+        <section>
+          <p>{this.props.quiz_questions.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            <li>{this.props.quiz_questions.answer_options[0]}</li>
+          </ul>
+        </section>
+      </main>
+    
+      )}
+   
+}
+
+export default QuizQuestion
