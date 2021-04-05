c1ff6a51d03004871fac68e33e1d2e3e36bd4a3d
d-marques <david.marques@my.istec.pt>
2021-04-05 20:49:17 +0100

    PluralSight_8

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,12 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 };
   }
 
+  handleResetClick() {
+    this.setState((state) => {
+      return { quiz_position: 1 };
+    });
+  }
+
   showNextQuestion() {
     this.setState((state) => {
       return { quiz_position: state.quiz_position + 1 };
@@ -22,7 +28,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             quiz_question={
